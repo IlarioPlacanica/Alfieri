806ffc425b0be420b67b1a994a0253c4e70b6ef0
Ilario Placanica <ilario.placanica1999@gmail.com>
2026-04-10 17:09:11 +0200

    Update urban-viewer.js

--- assets/js/urban-viewer.js
+++ assets/js/urban-viewer.js
@@ -84,7 +84,7 @@ async function initUrbanViewer() {
   const boundingSphere = Cesium.BoundingSphere.fromPoints(hierarchy.positions);
   const orbitTarget = boundingSphere.center;
 
-  let orbitHeading = Cesium.Math.toRadians(55);
+  let orbitHeading = Cesium.Math.toRadians(140);
   let orbitPitch = Cesium.Math.toRadians(-35);
   let orbitRange = 70;
 
